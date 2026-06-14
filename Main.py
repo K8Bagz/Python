e2d2e226712ade680bb4b1b68e6afaad87c3438c
K8Bagz <katebagwan@gmail.com>
2026-06-14 17:41:19 +0200

    tests ordered

--- Main.py
+++
@@ -1,8 +0,0 @@
-from power import power
-
-base = int(input("Enter the base: "))
-exponent = int(input("Enter the exponent: "))
-
-result = power(base, exponent)
-
-print(result)
--- app.py
+++ app.py
@@ -4,16 +4,21 @@ from commands.power import run as power
 from commands.multiply import run as multiply
 #from commands.reverse import run as reverse
 
-commands = {
-    "power": power,
-    "multiply": multiply,
-#    "reverse": reverse
-}
+def run():
+    commands = {
+        "power": power,
+        "multiply": multiply,
+    #    "reverse": reverse
+    }
 
-command = sys.argv[1] if len(sys.argv) > 1 else None
+    command = sys.argv[1] if len(sys.argv) > 1 else None
 
-if command not in commands:
-    print("Unknown command.")
-    exit()
+    if command not in commands:
+        print("Unknown command.")
+        exit()
 
-commands[command]()
+    commands[command]()
+
+# prevent potential files importing app to run it automatically
+if __name__ == "__main__" :
+    run()
